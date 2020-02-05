9ad6073972342ba97fe1a5fe261e5819b6b8d577
Larissa <>
2020-02-05 11:30:53 -0300

    add new file named LearningList

---
+++ LearningList/LearningList/EngagedCell.swift
@@ -0,0 +1,24 @@
+//
+//  EngagedCell.swift
+//  LearningList
+//
+//  Created by Larissa Diniz  on 05/02/20.
+//  Copyright © 2020 Larissa Diniz . All rights reserved.
+//
+
+import UIKit
+
+class EngagedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LearningList/LearningList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningList
+//
+//  Created by Larissa Diniz  on 05/02/20.
+//  Copyright © 2020 Larissa Diniz . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
